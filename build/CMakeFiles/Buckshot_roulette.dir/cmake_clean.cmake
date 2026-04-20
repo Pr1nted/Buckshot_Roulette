aac557a34adf22e0c4376f6c7d28551b8cd4c71e
Vladyslav Yavdoshenko <yavdovla@fit.cvut.cz>
2026-04-20 21:51:31 +0200

    Initial commit

---
+++ build/CMakeFiles/Buckshot_roulette.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Buckshot_roulette"
+  "Buckshot_roulette.pdb"
+  "CMakeFiles/Buckshot_roulette.dir/main.c.o"
+  "CMakeFiles/Buckshot_roulette.dir/main.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Buckshot_roulette.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Buckshot_roulette.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Buckshot_roulette.
+# This may be replaced when dependencies are built.
